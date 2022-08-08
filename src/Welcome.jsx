146137401592d60cd364f774e76badd484e62359
周康 <kang.zhou01@hand-china.com>
2022-08-08 22:35:14 +0800

    first commit

---
+++ src/Welcome.jsx
@@ -0,0 +1,13 @@
+import React,{Component} from "react";
+import "./App.css"
+
+
+
+export default class Welcome extends Component{
+
+        render() {
+                return <h2 >Hello 小文,<span className="love">❤❤</span></h2>
+        }
+
+}
+
